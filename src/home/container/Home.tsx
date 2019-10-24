a591cfa3308c2d1c9c4e5bda7f593c9bc3dbffca
Pascal Canuel <pascalcanuel@hotmail.com>
2019-10-24 00:32:28 -0400

    bluetooth works!

--- src/home/container/Home.tsx
+++ src/home/container/Home.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Colors } from '../../main/styles/Template';
 import BluetoothSerial from 'react-native-bluetooth-serial';
@@ -14,6 +14,7 @@ interface State {
   devices: Device[];
   selectedDevice: Device | null;
   isConnected: boolean;
+  disableConnectionBtn: boolean;
 }
 
 export default class Home extends Component<{}, State> {
@@ -23,37 +24,41 @@ export default class Home extends Component<{}, State> {
     this.state = {
       devices: [],
       selectedDevice: null,
-      isConnected: false
+      isConnected: false,
+      disableConnectionBtn: false
     };
   }
 
   scan = () => {
-    console.log('list');
-    BluetoothSerial.list().then((devices: Device[]) => {
-      this.setState({devices}, () => console.log(this.state.devices));
-      console.log(devices);
-    });
-}
+    BluetoothSerial.list().then((devices: Device[]) => this.setState({devices}) );
+  }
   
   setSelectedDevice = (device: Device) => this.setState({selectedDevice: device})
 
   toggleConnection = () => {
-    console.log('toggle connection');
+    this.setState({disableConnectionBtn: true});
     if (this.state.isConnected) {
       BluetoothSerial.disconnect()
         .then(() => {
-          this.setState({isConnected: false});
+          this.setState({
+            isConnected: false,
+            disableConnectionBtn: false,
+            devices: [...this.state.devices] // fix to reload flatlist and update selected device style
+          });
           BluetoothSerial.isConnected().then((res: boolean) => console.log(res));
         });
     } else {
-      console.log('try connection');
       BluetoothSerial.connect(this.state.selectedDevice!.id)
         .then(() => {
-          this.setState({isConnected: true});
-          BluetoothSerial.isConnected().then((res: boolean) => console.log(res));
+          this.setState({
+            isConnected: true, 
+            disableConnectionBtn: false,
+            devices: [...this.state.devices]
+          });
         });
     }
   }
+
   render() {
     return (
       <View style={styles.container}>
@@ -61,13 +66,22 @@ export default class Home extends Component<{}, State> {
           <Text style={styles.searchTxt}>Trouver un promeneur</Text>
           <Icon style={styles.searchIcon} name="bluetooth" size={30} color={Colors.primary}/>
         </TouchableOpacity>
-        {this.state.devices.map(device => 
-          <TouchableOpacity onPress={() => this.setSelectedDevice(device)} activeOpacity={0.7}>
-            <Text>{device.name}</Text>
-          </TouchableOpacity>
-        )}
+        <FlatList
+          data={this.state.devices}
+          renderItem={({item}) =>
+            <TouchableOpacity 
+              style={this.state.selectedDevice && item.id === this.state.selectedDevice.id && this.state.isConnected? styles.connectedDeviceBtn : styles.deviceBtn} 
+              onPress={() => this.setSelectedDevice(item)} 
+              activeOpacity={0.7}
+            >
+              <Text style={styles.deviceTxt}>{item.name}</Text>
+            </TouchableOpacity>
+          }
+          keyExtractor={item => item.id}
+          style={styles.deviceList}
+        />
         {this.state.selectedDevice &&
-          <TouchableOpacity style={styles.connectionBtn} onPress={this.toggleConnection} activeOpacity={0.7}>
+          <TouchableOpacity style={styles.connectionBtn} onPress={this.toggleConnection} disabled={this.state.disableConnectionBtn} activeOpacity={0.7}>
             <Text style={styles.connectionTxt}>{this.state.isConnected? "Déconnecter" : "Connecter"}</Text>
           </TouchableOpacity>
         }
@@ -82,7 +96,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
   searchBtn: {
     flexDirection: 'row',
@@ -93,21 +106,43 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     paddingTop: 10,
     paddingBottom: 10,
+    marginTop: 20,
+    margin: 5,
     width: btnWidth,
   },
   searchTxt: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
     color: Colors.primary,
   },
   searchIcon : {
     marginLeft: 10,
   },
-  connectionBtn: {
+  deviceList: {
+    maxHeight: Dimensions.get('window').height * 0.65,
+  },
+  deviceBtn: {
     width: btnWidth,
-    backgroundColor: 'red',
+  },
+  connectedDeviceBtn: {
+    width: btnWidth,
+    backgroundColor: Colors.secondary,
+  },
+  deviceTxt: {
+    fontSize: 20,
+  },
+  connectionBtn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    paddingTop: 10,
+    paddingBottom: 10,
+    width: btnWidth,
+    margin: 5,
+    backgroundColor: Colors.primaryLight,
   },
   connectionTxt: {
-    fontSize: 20,
+    fontSize: 24,
+    color: 'white',
   },
 })
